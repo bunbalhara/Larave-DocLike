78e9d8391a22d65f02186d7ce4f80f811720697b
bunbalhara <nanjingzhe425@gmail.com>
2020-08-27 01:52:21 +0800

    asggg

--- storage/framework/views/d8b54c06b91627f99e8be7f4f89708191291d803.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('main'); ?>
-    <div id="app"></div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('frontend.layouts.find', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\xampp\htdocs\doclike\resources\views/frontend/job/create.blade.php ENDPATH**/ ?>
