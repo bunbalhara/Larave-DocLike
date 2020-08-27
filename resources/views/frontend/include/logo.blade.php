99fcea0b6145e17314818dc104b9427b455b38bd
bunbalhara <nanjingzhe425@gmail.com>
2020-08-27 21:41:28 +0800

    commit 7

--- resources/views/frontend/include/logo.blade.php
+++ resources/views/frontend/include/logo.blade.php
@@ -2,7 +2,7 @@
     <div style="display: flex; flex-direction: row; justify-content: center; align-items: center; width: max-content">
         <img src="{{asset('assets/images/logo.png')}}" alt="logo" style="width: 36px; height: 36px;">
         <div style="font-size: 24px; font-family: Lato-Black-Italic; font-weight: bold; font-style: italic; color: #169bb9; margin-bottom: 3px">{{__("Doctors")}}</div>
-        <div style="font-size: 22px; font-family: Lato-Regular; font-weight: normal; color: #555555;margin-right: -2px; margin-left: 1px; margin-bottom: 3px">{{__("in")}}</div>
+        <div style="font-size: 24px; font-family: Lato-Regular; font-weight: normal; color: #555555; margin-bottom: 3px">{{__("in")}}</div>
         <div style="font-size: 24px; font-family: Lato-Black; font-weight: bold; color: #c44034; margin-bottom: 3px">{{__(session()->get('country','France'))}}</div>
     </div>
 </a>
